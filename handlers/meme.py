f9ba3de22e057b12ebfbd28826810a2d3d90db63
Aleksandr Bukhalo <a@bukhalo.com>
2021-09-10 00:30:25 +0300

    Some changes

--- handlers/meme.py
+++ handlers/meme.py
@@ -5,7 +5,7 @@ from utils import decision
 
 @dp.message_handler(
     filters.Text(equals=['дока', 'доку', 'документация', 'документацию'], ignore_case=True),
-    chat_type=types.ChatType.SUPERGROUP
+    chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP]
 )
 async def doka(msg: types.Message):
     await msg.answer('Доку профессора писали наверное')
@@ -13,7 +13,7 @@ async def doka(msg: types.Message):
 
 @dp.message_handler(
     filters.Text(equals=['пидор', 'пидорас'], ignore_case=True),
-    chat_type=types.ChatType.SUPERGROUP
+    chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP]
 )
 async def pidor(msg: types.Message):
     await msg.answer('тотарен')
@@ -21,7 +21,7 @@ async def pidor(msg: types.Message):
 
 @dp.message_handler(
     filters.Text(equals=['тотарен', 'тотарин', 'толик', 'толян', 'еболик'], ignore_case=True),
-    chat_type=types.ChatType.SUPERGROUP
+    chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP]
 )
 async def totaren(msg: types.Message):
     await msg.answer('пидор')
@@ -29,14 +29,14 @@ async def totaren(msg: types.Message):
 
 @dp.message_handler(
     filters.Text(equals=['ярик', 'ярек', 'ярослав'], ignore_case=True),
-    chat_type=types.ChatType.SUPERGROUP
+    chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP]
 )
 async def yarek(msg: types.Message):
     if decision(0.5):
         await msg.answer('Вы всё ещё готовите на огне @yaroslav_y? Тогда мы идём к вам.')
 
 
-@dp.message_handler(chat_type=types.ChatType.SUPERGROUP)
+@dp.message_handler(chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
 async def toopa(msg: types.Message):
-    if decision(0.05):
+    if decision(0.025):
         await msg.reply('ТУПА АЛЕГАНТОР))))))')
