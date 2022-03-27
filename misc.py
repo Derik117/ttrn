6e14d8a8c2b88bbcb28d98473bd95548e3f0acd3
Gusev Sergey <gusew.ser@gmail.com>
2022-03-27 14:25:58 +0300

    fix bot token

--- misc.py
+++ misc.py
@@ -1,9 +1,9 @@
 import logging
 
 from aiogram import Bot, Dispatcher
-from config import TOKEN
+from config import config
 
-bot = Bot(token=TOKEN)
+bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot=bot)
 logging.basicConfig(level=logging.DEBUG)
 
